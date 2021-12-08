2f3a0271ce90a0fe86fa28ab0702ed934221462e
Arken <Arken.Sarsenov@outlook.com>
2021-12-09 02:12:57 +0500

    Initial Commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by Arken Sarsenov on 09.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
